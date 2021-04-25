038bc56a84d29dfe31a41513b4590b04a1a4e2bb
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2021-04-25 16:44:26 +0200

    Install MaterialDesign and create Login and Update Windows

---
+++ BillsDesktopApp/AuthWindows/Login.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Bills.DL;
+using System.Linq;
+using System.Windows;
+using static Utilities.Utilities.Utilities;
+
+namespace Bills
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private readonly BillsContext _context;
+
+        private readonly UnitOfWork unitOfWork;
+        public Login(BillsContext Context)
+        {
+            _context = Context;
+            unitOfWork = new UnitOfWork(_context);
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var user = unitOfWork.Repository<BL.Models.Users>().Find(u => u.UserName.ToLower() == txtUserName.Text.ToLower())
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                MessageBox.Show("خطأ", "قمت بإدخال اسم مستخدم خطأ", MessageBoxButton.OK, MessageBoxImage.Error); ;
+
+            }
+
+            else
+            {
+                var isPwCorrect = UnHash(txtPassword.Password, user.Password);
+                if (isPwCorrect)
+                {
+                    Dashboard dashboard = new Dashboard(_context);
+                    dashboard.lblUserId.Content = user.ID;
+                    dashboard.lblWelcome.Content = " مرحباً " + user.UserName;
+                    Application.Current.MainWindow = dashboard;
+                    Close();
+                    dashboard.Show();
+                }
+                else
+                {
+                    MessageBox.Show("خطأ", "قمت بإدخال كلمة سر خطأ", MessageBoxButton.OK, MessageBoxImage.Error); ;
+                }
+
+            }
+
+
+
+        }
+
+        private void btnSignup_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp frmSignUp = new SignUp(_context);
+            frmSignUp.Show();
+            this.Close();
+        }
+    }
+}
---
+++ BillsDesktopApp/AuthWindows/SignUp.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Bills.DL;
+using Bills.ErrorMessages;
+using BL.Models;
+using System.Windows;
+using System.Windows.Controls;
+using static Utilities.Utilities.Utilities;
+
+namespace Bills
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        private readonly BillsContext _context;
+
+        private readonly UnitOfWork unitOfWork;
+        private bool IsValid = false;
+        public SignUp(BillsContext Context)
+        {
+            _context = Context;
+            unitOfWork = new UnitOfWork(_context);
+            InitializeComponent();
+            lblUserNameErrorMessage.Content = UserNameErrorMessages.UserNameMaxLength;
+            lblConfirmPasswordErrorMessage.Content = PasswordErrorMessages.PasswordConfirm;
+            lblPasswordErrorMessage.Content = PasswordErrorMessages.Password;
+        }
+
+        private void txtUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtUserName.Text.Length > 15 || txtUserName.Text.Length < 4)
+            {
+                lblUserNameErrorMessage.Visibility = Visibility.Visible;
+                IsValid = false;
+            }
+            else
+            {
+                lblUserNameErrorMessage.Visibility = Visibility.Hidden;
+                IsValid = true;
+
+            }
+        }
+
+        private void txtPasswordConfirm_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtPasswordConfirm.Password.ToString() != txtPassword.Password.ToString())
+            {
+                lblConfirmPasswordErrorMessage.Visibility = Visibility.Visible;
+                IsValid = false;
+            }
+            else
+            {
+                lblConfirmPasswordErrorMessage.Visibility = Visibility.Hidden;
+                IsValid = true;
+
+            }
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password.ToString().Length < 3)
+            {
+                lblPasswordErrorMessage.Visibility = Visibility.Visible;
+                IsValid = false;
+            }
+            else
+            {
+                lblPasswordErrorMessage.Visibility = Visibility.Hidden;
+                IsValid = true;
+            }
+        }
+
+        private void btnSignup_Click(object sender, RoutedEventArgs e)
+        {
+
+            var hashedPw = Hash(txtPassword.Password);
+
+
+            if (!IsValid)
+            {
+                MessageBox.Show("خطأ بالتسجيل", "برجاء إدخال بيانات صالحة", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            else
+            {
+                unitOfWork.Repository<BL.Models.Users>().Add(new Users
+                {
+                    UserName = txtUserName.Text,
+                    Password = hashedPw
+                });
+            }
+
+            int result = unitOfWork.Complete();
+
+            if (result < 1)
+            {
+                MessageBox.Show("حدث خطأ بالتسجيل", "خطأ بالتسجيل", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+
+            else
+            {
+                Login frmLogin = new Login(_context);
+                Application.Current.MainWindow = frmLogin;
+                Close();
+                frmLogin.Show();
+            }
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Login frmLogin = new Login(_context);
+            frmLogin.Show();
+            Application.Current.MainWindow = frmLogin;
+            this.Close();
+        }
+    }
+}
