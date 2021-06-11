061f598cb7d47f51158c4bd768290c38ac18f3cb
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2021-06-11 19:34:41 +0200

    Create Database if not exist in the app run

--- BillsDesktopApp/App.xaml.cs
+++ BillsDesktopApp/App.xaml.cs
@@ -1,7 +1,8 @@
-﻿using com.rusanu.DBUtil;
+﻿using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace BillsDesktopApp
@@ -15,7 +16,36 @@ namespace BillsDesktopApp
 
         public App()
         {
-           
-        }       
+            using (SqlConnection sqlConnection = new(connectionString))
+            {
+                string filePath = GetDestinationPath("BillsCore.sql", @"Static");
+                string script = File.ReadAllText(filePath);
+                IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
+                                     RegexOptions.Multiline | RegexOptions.IgnoreCase);
+
+                sqlConnection.Open();
+                foreach (string commandString in commandStrings)
+                {
+                    if (commandString.Trim() != "")
+                    {
+                        using (var command = new SqlCommand(commandString, sqlConnection))
+                        {
+                            command.ExecuteNonQuery();
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private static string GetDestinationPath(string filename, string foldername)
+        {
+            string appStartPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+            appStartPath = string.Format(appStartPath + "\\{0}\\" + filename, foldername);
+            return appStartPath;
+        }
+
     }
 }
--- BillsDesktopApp/Static/BillsCore.sql
+++ BillsDesktopApp/Static/BillsCore.sql
@@ -1,10 +1,8 @@
 USE [master]
 GO
-
-drop database if exists BillsCore
-GO
-
-/****** Object:  Database [BillsCore]    Script Date: 11/06/2021 02:08:19 م ******/
+/****** Object:  Database [BillsCore]    Script Date: 11/06/2021 07:24:06 م ******/
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'BillsCore')
+BEGIN
 CREATE DATABASE [BillsCore]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -12,6 +10,7 @@ CREATE DATABASE [BillsCore]
  LOG ON 
 ( NAME = N'BillsCore_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\BillsCore_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
  WITH CATALOG_COLLATION = DATABASE_DEFAULT
+END
 GO
 ALTER DATABASE [BillsCore] SET COMPATIBILITY_LEVEL = 150
 GO
@@ -86,11 +85,13 @@ ALTER DATABASE [BillsCore] SET QUERY_STORE = OFF
 GO
 USE [BillsCore]
 GO
-/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 11/06/2021 07:24:06 م ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[__EFMigrationsHistory]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[__EFMigrationsHistory](
 	[MigrationId] [nvarchar](150) NOT NULL,
 	[ProductVersion] [nvarchar](32) NOT NULL,
@@ -99,12 +100,15 @@ CREATE TABLE [dbo].[__EFMigrationsHistory](
 	[MigrationId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+END
 GO
-/****** Object:  Table [dbo].[Companies]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Table [dbo].[Companies]    Script Date: 11/06/2021 07:24:06 م ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Companies]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[Companies](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
@@ -116,12 +120,15 @@ CREATE TABLE [dbo].[Companies](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
 GO
-/****** Object:  Table [dbo].[Customers]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Table [dbo].[Customers]    Script Date: 11/06/2021 07:24:06 م ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Customers]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[Customers](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
@@ -133,12 +140,15 @@ CREATE TABLE [dbo].[Customers](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
 GO
-/****** Object:  Table [dbo].[OrderDetails]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Table [dbo].[OrderDetails]    Script Date: 11/06/2021 07:24:06 م ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[OrderDetails]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[OrderDetails](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[OrderId] [int] NOT NULL,
@@ -150,12 +160,15 @@ CREATE TABLE [dbo].[OrderDetails](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+END
 GO
-/****** Object:  Table [dbo].[Orders]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Table [dbo].[Orders]    Script Date: 11/06/2021 07:24:06 م ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[Orders](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[CustomerId] [int] NOT NULL,
@@ -167,12 +180,15 @@ CREATE TABLE [dbo].[Orders](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
 GO
-/****** Object:  Table [dbo].[Products]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Table [dbo].[Products]    Script Date: 11/06/2021 07:24:06 م ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Products]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[Products](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
@@ -183,12 +199,15 @@ CREATE TABLE [dbo].[Products](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
 GO
-/****** Object:  Table [dbo].[Users]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 11/06/2021 07:24:06 م ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[Users](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[UserName] [nvarchar](15) NOT NULL,
@@ -199,65 +218,81 @@ CREATE TABLE [dbo].[Users](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
 GO
-/****** Object:  Index [IX_OrderDetails_OrderId]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Index [IX_OrderDetails_OrderId]    Script Date: 11/06/2021 07:24:06 م ******/
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[OrderDetails]') AND name = N'IX_OrderDetails_OrderId')
 CREATE NONCLUSTERED INDEX [IX_OrderDetails_OrderId] ON [dbo].[OrderDetails]
 (
 	[OrderId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_OrderDetails_ProductId]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Index [IX_OrderDetails_ProductId]    Script Date: 11/06/2021 07:24:06 م ******/
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[OrderDetails]') AND name = N'IX_OrderDetails_ProductId')
 CREATE NONCLUSTERED INDEX [IX_OrderDetails_ProductId] ON [dbo].[OrderDetails]
 (
 	[ProductId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_Orders_CustomerId]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Index [IX_Orders_CustomerId]    Script Date: 11/06/2021 07:24:06 م ******/
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = N'IX_Orders_CustomerId')
 CREATE NONCLUSTERED INDEX [IX_Orders_CustomerId] ON [dbo].[Orders]
 (
 	[CustomerId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_Orders_UserId]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Index [IX_Orders_UserId]    Script Date: 11/06/2021 07:24:06 م ******/
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = N'IX_Orders_UserId')
 CREATE NONCLUSTERED INDEX [IX_Orders_UserId] ON [dbo].[Orders]
 (
 	[UserId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_Users_CompanyId]    Script Date: 11/06/2021 02:08:20 م ******/
+/****** Object:  Index [IX_Users_CompanyId]    Script Date: 11/06/2021 07:24:06 م ******/
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND name = N'IX_Users_CompanyId')
 CREATE NONCLUSTERED INDEX [IX_Users_CompanyId] ON [dbo].[Users]
 (
 	[CompanyId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_OrderDetails_Orders_OrderId]') AND parent_object_id = OBJECT_ID(N'[dbo].[OrderDetails]'))
 ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Orders_OrderId] FOREIGN KEY([OrderId])
 REFERENCES [dbo].[Orders] ([ID])
 ON DELETE CASCADE
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_OrderDetails_Orders_OrderId]') AND parent_object_id = OBJECT_ID(N'[dbo].[OrderDetails]'))
 ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Orders_OrderId]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_OrderDetails_Products_ProductId]') AND parent_object_id = OBJECT_ID(N'[dbo].[OrderDetails]'))
 ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Products_ProductId] FOREIGN KEY([ProductId])
 REFERENCES [dbo].[Products] ([ID])
 ON DELETE CASCADE
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_OrderDetails_Products_ProductId]') AND parent_object_id = OBJECT_ID(N'[dbo].[OrderDetails]'))
 ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Products_ProductId]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Orders_Customers_CustomerId]') AND parent_object_id = OBJECT_ID(N'[dbo].[Orders]'))
 ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers_CustomerId] FOREIGN KEY([CustomerId])
 REFERENCES [dbo].[Customers] ([ID])
 ON DELETE CASCADE
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Orders_Customers_CustomerId]') AND parent_object_id = OBJECT_ID(N'[dbo].[Orders]'))
 ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers_CustomerId]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Orders_Users_UserId]') AND parent_object_id = OBJECT_ID(N'[dbo].[Orders]'))
 ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Users_UserId] FOREIGN KEY([UserId])
 REFERENCES [dbo].[Users] ([ID])
 ON DELETE CASCADE
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Orders_Users_UserId]') AND parent_object_id = OBJECT_ID(N'[dbo].[Orders]'))
 ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Users_UserId]
 GO
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Users_Companies_CompanyId]') AND parent_object_id = OBJECT_ID(N'[dbo].[Users]'))
 ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_Companies_CompanyId] FOREIGN KEY([CompanyId])
 REFERENCES [dbo].[Companies] ([ID])
 ON DELETE CASCADE
 GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Users_Companies_CompanyId]') AND parent_object_id = OBJECT_ID(N'[dbo].[Users]'))
 ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_Companies_CompanyId]
 GO
 USE [master]
